365172b8273f12a4192158b5cc840b0839aba5de
Emerson Sormany <sormanyemerson@gmail.com>
2024-10-29 17:42:33 +0000

    correção

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,8 @@ function tabuada(operacao) {
     switch (operacao) {
         case 'soma':
             for(let i = 0; i < 10; i++){
-                for(let j = 0; i < 10; j++){
-                    document.getElementById("tabuada-soma").innerHTML += ""
+                for(let j = 0; j < 10; j++){
+                    document.getElementById("tabuada-soma").innerHTML += `${i} + ${j} = ${i + j} <br>`
                 }
             }
             break
