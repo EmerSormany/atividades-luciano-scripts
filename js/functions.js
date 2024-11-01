5693aaafd3419b43c9fd32fc832a68d6cbfbcc27
Emerson Sormany <sormanyemerson@gmail.com>
2024-11-01 23:55:23 +0000

    Resolução da atividade

--- js/functions.js
+++ js/functions.js
@@ -3,12 +3,34 @@ function tabuada(operacao) {
         case 'soma':
             for(let i = 0; i < 10; i++){
                 for(let j = 0; j < 10; j++){
-                    document.getElementById("tabuada-soma").innerHTML += `${i} + ${j} = ${i + j} <br>`
+                    document.getElementById("soma").innerHTML += `${i} + ${j} = ${(i + j).toFixed(4)} <br>`
                 }
+                document.getElementById("soma").innerHTML += `<br>`
             }
             break
-        case 'subtracao':            
+        case 'subtracao': 
+            for(let i = 0; i < 10; i++){
+                for(let j = 0; j < 10; j++){
+                    document.getElementById("subtracao").innerHTML += `${i} - ${j} = ${(i - j).toFixed(4)} <br>`
+                }
+                document.getElementById("subtracao").innerHTML += `<br>`
+            }  
+            break         
         case 'multiplicacao':
+            for(let i = 0; i < 10; i++){
+                for(let j = 0; j < 10; j++){
+                    document.getElementById("multiplicacao").innerHTML += `${i} x ${j} = ${(i * j).toFixed(4)} <br>`
+                }
+                document.getElementById("multiplicacao").innerHTML += `<br>`
+            } 
+            break
         case 'divisao':
+            for(let i = 0; i < 10; i++){
+                for(let j = 0; j < 10; j++){
+                    document.getElementById("divisao").innerHTML += `${i} ÷ ${j} = ${(i / j).toFixed(4)} <br>`
+                }
+                document.getElementById("divisao").innerHTML += `<br>`
+            } 
+            break
     }
 }
